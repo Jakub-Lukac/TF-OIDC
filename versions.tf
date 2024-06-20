3072e7dc1521fe0bd2c268070c9b64c935bb4e3a
Jakub-Lukac <jakub.lukac81@gmail.com>
2024-06-20 12:39:13 +0200

    random provider added

--- versions.tf
+++ versions.tf
@@ -4,6 +4,11 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "~> 3.8.9"
     }
+
+    random = {
+        source = "hashicorp/random"
+        version = "3.4.3"
+    }
   }
 
   backend "azurerm" {
